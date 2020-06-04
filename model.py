e9e12df0bf367b5a5f6d0ac2f995a97321b5be7a
MH <436077853@qq.com>
2020-06-04 08:37:37 +0200

    trained, ran successfully

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import os
 import csv
 from keras.models import Sequential, Model
-from keras.layers import Lambda, Cropping2D
+from keras.layers import Lambda, Cropping2D, Flatten, Dense, Convolution2D
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import sklearn
@@ -19,14 +19,14 @@ with open('../Drive_Manual/driving_log.csv') as csvfile:
     for line in reader:
         samples.append(line)
 
-random.shuffle(sample) # shuffle the images after read them all, and before the test_train split, m
+random.shuffle(samples) # shuffle the images after read them all, and before the test_train split, m
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
+        random.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
             images = []
@@ -37,8 +37,8 @@ def generator(samples, batch_size=32):
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
-                images.append(np.fliplr(center_image)) #flip the image, m
-                angles.append(-center_angle) # the steering angle reverse, m
+#                 images.append(np.fliplr(center_image)) #flip the image, because it is not suit for the same croping methode, commented out, m
+#                 angles.append(-center_angle) # the steering angle reverse, because it is not suit for the same croping methode, commented out, m
                 
 
             # trim image to only see section with road
@@ -58,26 +58,32 @@ validation_generator = generator(validation_samples, batch_size=batch_size)
 ch, row, col = 3, 80, 320  # Trimmed image format
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-model.add(Lambda(lambda x: x/127.5 - 1.,
-        input_shape=(ch, row, col),
-        output_shape=(ch, row, col)))
-model.add(... finish defining the rest of your model architecture here ...) # TODO, m
-
+# model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3))) # same meaning with x/127.5-1
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3))) #70,25
+model.add(Lambda(lambda x: x/255.0 - 0.5))
+model.add(Convolution2D(24,5,5, subsample=(2,2),activation="relu"))
+model.add(Convolution2D(36,5,5, subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5, subsample=(2,2),activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, /
-            steps_per_epoch=ceil(len(train_samples)/batch_size), /
-            validation_data=validation_generator, /
-            validation_steps=ceil(len(validation_samples)/batch_size), /
-            epochs=5, verbose=1)
+model.fit_generator(train_generator,\
+            steps_per_epoch=np.ceil(len(train_samples)/batch_size), \
+            validation_data=validation_generator, \
+            validation_steps=np.ceil(len(validation_samples)/batch_size), \
+            epochs=2, verbose=1)
 
-model.save(model.h5)
+model.save('model.h5')
 
 # produce the visualization
 
-history_object = model.fit_generator(train_generator, samples_per_epoch = len(train_samples), validation_data = validation_generator, nb_val_samples = len(validation_samples), nb_epoch=5, verbose=1)
+history_object = model.fit_generator(train_generator, samples_per_epoch = len(train_samples), validation_data = validation_generator, nb_val_samples = len(validation_samples), nb_epoch=2, verbose=1)
 
 ### print the keys contained in the history object
 print(history_object.history.keys())
